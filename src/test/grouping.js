1e05c72e9f618402aab38c5c22375de639fdb4bc
h3rby7 <herbielutz@gmx.net>
2023-01-02 15:08:28 +0100

    finished tests for shotsHaveSameCharactersInCostumes

--- src/test/grouping.js
+++ src/test/grouping.js
@@ -34,4 +34,60 @@ export function addTests_grouping(TESTS) {
     return TestHelpers.printTestResult(testName, msg);
   });
 
+  TESTS.add("GROUPING SAME CHARACTERS SAME COSTUMES -> GIVEN: [A, B] and [B, A] EXPECTING true", (testName) => {
+    // SETUP
+    const cA = [TestHelpers.createCharacterInCostume("A", "tiger"), TestHelpers.createCharacterInCostume("B", "dinosaur")];
+    const cB = [TestHelpers.createCharacterInCostume("B", "dinosaur"), TestHelpers.createCharacterInCostume("A", "tiger")];
+    // EXECUTION
+    const result = shotsHaveSameCharactersInCostumes(cA, cB);
+    // INTERPRETATION
+    let msg = null;
+    if (!result) {
+      msg = "Should have same characters (only order is different)";
+    }
+    return TestHelpers.printTestResult(testName, msg);
+  });
+
+  TESTS.add("GROUPING SAME CHARACTERS SAME COSTUMES -> GIVEN: [A] and [] EXPECTING false", (testName) => {
+    // SETUP
+    const cA = [TestHelpers.createCharacterInCostume("A", "tiger")];
+    const cB = [];
+    // EXECUTION
+    const result = shotsHaveSameCharactersInCostumes(cA, cB);
+    // INTERPRETATION
+    let msg = null;
+    if (result) {
+      msg = "Should be different as A is not in the second shot";
+    }
+    return TestHelpers.printTestResult(testName, msg);
+  });
+
+  TESTS.add("GROUPING SAME CHARACTERS SAME COSTUMES -> GIVEN: [] and [A] EXPECTING false", (testName) => {
+    // SETUP
+    const cA = [];
+    const cB = [TestHelpers.createCharacterInCostume("A", "tiger")];
+    // EXECUTION
+    const result = shotsHaveSameCharactersInCostumes(cA, cB);
+    // INTERPRETATION
+    let msg = null;
+    if (result) {
+      msg = "Should be different as A is not in the first shot";
+    }
+    return TestHelpers.printTestResult(testName, msg);
+  });
+
+  TESTS.add("GROUPING SAME CHARACTERS SAME COSTUMES -> GIVEN: [A] and [B] EXPECTING false", (testName) => {
+    // SETUP
+    const cA = [TestHelpers.createCharacterInCostume("A", "tiger")];
+    const cB = [TestHelpers.createCharacterInCostume("B", "dinosaur")];
+    // EXECUTION
+    const result = shotsHaveSameCharactersInCostumes(cA, cB);
+    // INTERPRETATION
+    let msg = null;
+    if (result) {
+      msg = "Should be different as first shot has 'A', second shot has 'B'";
+    }
+    return TestHelpers.printTestResult(testName, msg);
+  });
+
 }
